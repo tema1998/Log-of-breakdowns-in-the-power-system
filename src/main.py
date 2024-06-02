dd8e0db10b3720a2ad0fbca8bd5f71daa21343b6
tema1998 <104845356+tema1998@users.noreply.github.com>
2024-06-02 15:07:30 +0300

    Create router.py breakdowns_log. Include router.py to main.py.

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from auth.base_config import auth_backend, fastapi_users, google_oauth_client
 from auth.schemas import UserRead, UserCreate
+from breakdowns_log.router import router as router_breakdowns_log
 
 app = FastAPI(title="Auth microservice")
 
@@ -17,9 +18,10 @@ app.include_router(
     tags=["auth"],
 )
 
-
 app.include_router(
     fastapi_users.get_oauth_router(google_oauth_client, auth_backend, "SECRET"),
     prefix="/auth/google",
     tags=["auth"],
 )
+
+app.include_router(router_breakdowns_log)
